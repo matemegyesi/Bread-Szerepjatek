592c4dcc9e1f0f8bc1f096af995a4c2e6e55acc0
Barna András Bence <barnabenceneu@gmail.com>
2023-02-27 14:15:43 +0100

    Interfacek mappa és Icollectible létrehozva

---
+++ Interfacek/Interfacek/Icollectible.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfacek
+{
+    interface Icollectible
+    {
+        void Collect();
+        void Drop();
+    }
+}
