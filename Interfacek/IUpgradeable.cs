0c8c0bff7b4d6f13ab5c9fe767c176d3bc0113e4
neugulyascsaba <75720679+neugulyascsaba@users.noreply.github.com>
2023-02-27 14:36:12 +0100

    Fejlesztés interface feltöltése

---
+++ Interfacek/IUpgradeable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface IUpgradeable
+    {
+
+        bool CanUpgrade { get; }
+        int UpgradeCost { get; }
+
+        void Upgrade();
+        
+    }
+}
