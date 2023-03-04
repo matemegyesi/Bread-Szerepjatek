1d3cfe59fe6cd419cd906b2066932d5ed88374be
chencsi <kevinkeeeh@gmail.com>
2023-03-04 15:57:46 +0100

    IEffect.cs hozzáadása

---
+++ interface/IEffect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace interfacek
+{
+    public interface IEffect
+    {
+        //---Bántó---
+        void Bleed(); //Vérzés
+        void Burn(); //Égés
+        void Blindness(); //Vakság
+        void Poison(); //Ázás ("savas eső")
+        void Weakness(); //Gyengeség
+        void Slowness(); //Lassúság
+        void Stun(); //Elkábítani
+
+        //---Védekező---
+        void Bandage(); //Kötszer
+        void HealthRegeneration(); //Élet regeneráció
+        void ManaRegeneration(); //Mana regeneráció
+    }
+}
+
