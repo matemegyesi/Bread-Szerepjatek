93df9d61ad0297223cbd15a80eb392a33f2a3939
Gulyás Csaba <neugulyascsaba@gmail.com>
2023-03-15 07:23:59 +0100

    Style class létrehozása, EnableStyling metódus

---
+++ PoP/PoP/classes/Style.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ansi;
+
+namespace PoP.classes
+{
+    internal class Style
+    {
+
+        public static void EnableStyling()
+        {
+            WindowsConsole.TryEnableVirtualTerminalProcessing();
+        }
+
+    }
+}
