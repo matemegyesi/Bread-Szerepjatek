7c536e925295b8a75fec07f4a199d83e98fda974
Gulyás Csaba <neugulyascsaba@gmail.com>
2023-04-01 21:04:41 +0200

    Location class létrehozása

---
+++ PoP/PoP/classes/Location.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoP.classes
+{
+    internal abstract class Location
+    {
+
+        public readonly int Id;
+        public int positionX { get; private set; }
+        public int positionY { get; private set; }
+
+        public Location(int id, int x, int y)
+        {
+            Id = id;
+            positionX = x;
+            positionY = y;
+        }
+
+    }
+}
