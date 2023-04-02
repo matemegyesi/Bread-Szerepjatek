71e2e97f2c94fe0efa4d5d7cdc83db2d95f389e1
Gulyás Csaba <neugulyascsaba@gmail.com>
2023-04-02 07:07:41 +0200

    Combat és Dialogue class

---
+++ PoP/PoP/classes/Combat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoP.classes
+{
+    internal class Combat : Location
+    {
+        public Combat(int id, int x, int y) : base(id, x, y)
+        {
+            
+        }
+    }
+}
---
+++ PoP/PoP/classes/Dialogue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoP.classes
+{
+    internal class Dialogue : Location
+    {
+        public Dialogue(int id, int x, int y) : base(id, x, y)
+        {
+
+        }
+    }
+}
