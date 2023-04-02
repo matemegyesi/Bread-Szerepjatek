003b69e6e6563a9064d87c522f333a8cc5214dc1
Takács Balázs <neu.takacs.balazs1@gmail.com>
2023-04-02 14:27:44 +0200

    Map osztály létrehozva

---
+++ PoP/PoP/classes/Map.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoP.classes
+{
+	class Map
+	{
+        public static List<Map> maps = new List<Map>();
+
+        public int CurrentLocation { get; set; }
+
+        public readonly string path;
+
+
+        public Dictionary<string, Location> locations = new Dictionary<string, Location>();
+
+        public Map(string path, int CLocation) {
+
+            this.path = path;
+            CurrentLocation = CLocation;
+
+            maps.Add(this);
+        }
+   } 
+}
