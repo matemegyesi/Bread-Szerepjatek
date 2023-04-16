fc2a6bd626a9ad8ec8221772612cf4be4f3d0e4a
Megyesi Máté <megyesimate@gmail.com>
2023-04-16 11:00:50 +0200

    kis hiba a szoveg kiiratassal

--- PoP/PoP/classes/Dialogue.cs
+++ PoP/PoP/classes/Dialogue.cs
@@ -28,6 +28,7 @@ namespace PoP.classes
 
         public override void LoadLocation()
         {
+            GameLoop.display.WipeTextBox();
             Map.CurrentLocation = this;
             GameLoop.Phase = GamePhase.DIALOGUE;
 
