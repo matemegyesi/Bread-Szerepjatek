3a4488468ad1a222796dc949bc373ca111cc41be
Megyesi Máté <megyesimate@gmail.com>
2023-04-21 20:41:05 +0200

    KeyboardInput doc

--- PoP/PoP/classes/KeyboardInput.cs
+++ PoP/PoP/classes/KeyboardInput.cs
@@ -9,15 +9,28 @@ namespace PoP.classes
 {
     class KeyboardInput
     {
+        ///<summary>
+        /// Allows keyboard input to be listened to in a separate thread.
+        ///</summary>
         public KeyboardInput()
         {
             Thread inputThread = new Thread(StartListening);
             inputThread.Start();
         }
-        
+
+        ///<summary>
+        ///A delegate for handling a key press event.
+        ///</summary>
         public delegate void OnKeyPressed(ConsoleKey key);
+
+        ///<summary>
+        ///An event that is triggered when a key is pressed.
+        ///</summary>
         static public OnKeyPressed KeyPressed;
 
+        ///<summary>
+        ///Listens for keyboard input and triggers the KeyPressed event.
+        ///</summary>
         public void StartListening()
         {
             while (GameLoop.Running)
