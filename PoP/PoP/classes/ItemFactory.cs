39417b6a2d5fc116ce0c28f1c2af8bdc705241b0
Megyesi Máté <megyesimate@gmail.com>
2023-04-21 20:38:07 +0200

    ItemFactory doc

--- PoP/PoP/classes/ItemFactory.cs
+++ PoP/PoP/classes/ItemFactory.cs
@@ -8,6 +8,14 @@ namespace PoP.classes
 {
     class ItemFactory
     {
+        ///<summary>
+        ///Creates a new item of the specified type with the given properties.
+        ///</summary>
+        ///<param name="type">The type of item to create (Weapon or Armor).</param>
+        ///<param name="name">The name of the item.</param>
+        ///<param name="damageOrDefense">The damage (if the item is a weapon) or defense (if the item is an armor) value of the item.</param>
+        ///<param name="slot">The slot where the item can be equipped.</param>
+        ///<returns>A new item of the specified type with the given properties.</returns>
         public static Item CreateItem(ItemType type, string name, float damageOrDefense, Slot slot)
         {
             Item item = null;
