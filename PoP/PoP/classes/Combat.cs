2ba9f1dc09342073c72b5070a65c9a58d7607e49
Megyesi Máté <megyesimate@gmail.com>
2023-04-21 15:35:13 +0200

    Combat doc

--- PoP/PoP/classes/Combat.cs
+++ PoP/PoP/classes/Combat.cs
@@ -7,12 +7,34 @@ using System.Threading.Tasks;
 
 namespace PoP.classes
 {
+    /// <summary>
+    /// Represents the different phases of combat.
+    /// </summary>
     enum CombatPhase
     {
+        /// <summary>
+        /// The loadout phase, where the player can customize their spells.
+        /// </summary>
         LOADOUT,
+
+        /// <summary>
+        /// The player turn phase, where the player can attack with spell or weapon.
+        /// </summary>
         PLAYER_TURN,
+
+        /// <summary>
+        /// The enemy turn phase, where the enemy takes action.
+        /// </summary>
         ENEMY_TURN,
+
+        /// <summary>
+        /// The lose phase, where the player loses the game.
+        /// </summary>
         LOSE,
+
+        /// <summary>
+        /// The win phase, where the player wins the game.
+        /// </summary>
         WIN
     }
 
@@ -22,6 +44,13 @@ namespace PoP.classes
 
         Enemy enemy; // kell majd rendes beolvasás
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Combat"/> class.
+        /// </summary>
+        /// <param name="id">The ID of the combat encounter.</param>
+        /// <param name="x">The X coordinate of the location of the combat encounter.</param>
+        /// <param name="y">The Y coordinate of the location of the combat encounter.</param>
+        /// <param name="path">The path to the combat encounter.</param>
         public Combat(int id, int x, int y, string path) : base(id, x, y)
         {
             this.id = id;
@@ -32,6 +61,9 @@ namespace PoP.classes
             enemy = new Enemy(/* Path majd */);
         }
 
+        /// <summary>
+        /// Sets the <see cref="Map"/>.CurrentLocation to this instance and starts the Combat
+        /// </summary>
         public override void LoadLocation()
         {
             GameLoop.display.WipeTextBox();
@@ -41,6 +73,9 @@ namespace PoP.classes
             Start();
         }
 
+        /// <summary>
+        /// Starts the combat encounter.
+        /// </summary>
         public override void Start()
         {
 
@@ -50,6 +85,9 @@ namespace PoP.classes
 
         }
 
+        /// <summary>
+        /// Ends the combat encounter.
+        /// </summary>
         public override void End()
         {
             KeyboardInput.KeyPressed -= KeyPressed;
@@ -58,6 +96,10 @@ namespace PoP.classes
             GameLoop.Phase = GamePhase.ADVENTURE;
         }
 
+        /// <summary>
+        /// Generates a list of strings containing information about the player's current state.
+        /// </summary>
+        /// <returns>A list of strings containing player information.</returns>
         private List<string> GetPlayerInfo()
         {
             List<string> playerInfo = new List<string>();
@@ -73,6 +115,10 @@ namespace PoP.classes
             return playerInfo;
         }
 
+        /// <summary>
+        /// Generates a list of strings containing information about the enemy's current state.
+        /// </summary>
+        /// <returns>A list of strings containing enemy information.</returns>
         private List<string> GetEnemyInfo()
         {
             List<string> enemyInfo = new List<string>();
@@ -87,6 +133,10 @@ namespace PoP.classes
             return enemyInfo;
         }
 
+        /// <summary>
+        /// Changes the current combat phase of the game to the specified new phase.
+        /// </summary>
+        /// <param name="newPhase">The new CombatPhase enumeration value to change the current phase to.</param>
         public void ChangeCombatPhase(CombatPhase newPhase)
         {
             combatPhase = newPhase;
@@ -95,20 +145,21 @@ namespace PoP.classes
             switch (combatPhase)
             {
                 case CombatPhase.LOADOUT:
-
+                    // Display the loadout selection screen
                     GameLoop.display.DrawString("Loadout selection", 4, 50);
                     GameLoop.display.DrawString("Begin encounter (SPACE)", 4, 52);
 
                     break;
 
                 case CombatPhase.PLAYER_TURN:
-
+                    // Display the player's turn screen
                     GameLoop.display.DrawString("Player's turn", 4, 50);
                     GameLoop.display.DrawString("Weapon attack (Q)", 4, 52);
                     GameLoop.display.DrawString("Use spell 1 (W)", 4, 53);
                     GameLoop.display.DrawString("Use spell 2 (E)", 4, 54);
                     GameLoop.display.DrawString("Use spell 3 (R)", 4, 55);
 
+                    // Display the player and enemy information
                     List<string> playerInfo = GetPlayerInfo();
                     for (int i = 0; i < playerInfo.Count; i++)
                     {
@@ -124,17 +175,18 @@ namespace PoP.classes
                     break;
 
                 case CombatPhase.ENEMY_TURN:
-
+                    // Display the enemy's turn screen
                     GameLoop.display.DrawString("Enemy's turn", 4, 50);
                     GameLoop.display.DrawString("Next turn (SPACE)", 4, 52);
 
+                    // Get the enemy's action and display it
                     string enemyAction = enemy.TakeAction();
                     GameLoop.display.DrawString(enemyAction, 40, 50);
 
                     break;
 
                 case CombatPhase.WIN:
-
+                    // Display the win screen and prompt to end encounter
                     GameLoop.display.DrawString("The player won!", 4, 50);
                     GameLoop.display.DrawString("End encounter (SPACE)", 4, 52);
                     // itt loot-ot kéne kapni
@@ -142,7 +194,7 @@ namespace PoP.classes
                     break;
 
                 case CombatPhase.LOSE:
-
+                    // Display the lose screen and prompt to end encounter
                     GameLoop.display.DrawString("The player lost!", 4, 50);
                     GameLoop.display.DrawString("End encounter (SPACE)", 4, 52);
                     // itt meg kéne hóni
