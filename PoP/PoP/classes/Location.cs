8da3aafb099b10efb39def47f0cc4bf4498a136a
Megyesi Máté <megyesimate@gmail.com>
2023-04-21 20:43:37 +0200

    Location doc

--- PoP/PoP/classes/Location.cs
+++ PoP/PoP/classes/Location.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace PoP.classes
 {
+    ///<summary>
+    /// The type of location, either dialogue or combat.
+    ///</summary>
     enum LocationType
     {
         DIALOGUE,
@@ -13,12 +16,34 @@ namespace PoP.classes
     }
     internal abstract class Location
     {
-
+        ///<summary>
+        /// The location ID.
+        ///</summary>
         public int id;
+
+        ///<summary>
+        /// The x-coordinate of the location on the game map.
+        ///</summary>
         public int positionX { get; set; }
+
+        ///<summary>
+        /// The y-coordinate of the location on the game map.
+        ///</summary>
         public int positionY { get; set; }
+
+        ///<summary>
+        /// A flag indicating whether the location has been completed or not.
+        ///</summary>
         public bool isCompleted { get; set; }
+
+        ///<summary>
+        /// The path of the location.
+        ///</summary>
         public string Path { get; set; }
+
+        ///<summary>
+        /// The name of the location.
+        ///</summary>
         public string Name { get; set; }
         public Location(int id, int x, int y)
         {
@@ -28,8 +53,19 @@ namespace PoP.classes
             isCompleted = false;
         }
 
-        public abstract void LoadLocation(); 
+        ///<summary>
+        /// Loads the location.
+        ///</summary>
+        public abstract void LoadLocation();
+
+        ///<summary>
+        /// Starts the location.
+        ///</summary>
         public abstract void Start();
+
+        ///<summary>
+        /// Ends the location.
+        ///</summary>
         public abstract void End();
 
     }
