66a1622c2fda64d6ca63b2e09705842899a34187
Megyesi Máté <megyesimate@gmail.com>
2023-04-21 20:29:59 +0200

    Inventory doc

--- PoP/PoP/classes/Inventory.cs
+++ PoP/PoP/classes/Inventory.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace PoP
 {
+    ///<summary>
+    /// An enumeration representing the different item slots in the inventory.
+    /// </summary>
     public enum Slot
     {
         HEAD,
@@ -17,11 +20,19 @@ namespace PoP
     }
     class Inventory
     {
-
+        ///<summary>
+        /// The maximum number of items that the inventory can hold.
+        /// </summary>
         public const int inventoryLimit = 40;
 
+        ///<summary>
+        /// The list of items in the inventory.
+        /// </summary>
         public static List<Item> inventory = new List<Item>();
 
+        ///<summary>
+        /// A dictionary of gear slots and their corresponding equipped items.
+        /// </summary>
         public static Dictionary<Slot, Item> gear = new Dictionary<Slot, Item>() {
             { Slot.HEAD, null},
             { Slot.CHEST, null},
