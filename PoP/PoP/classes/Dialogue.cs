283d6352aa49c3982f3e9f99838ac639c3f8e483
Megyesi Máté <megyesimate@gmail.com>
2023-04-21 20:13:41 +0200

    Dialogue doc

--- PoP/PoP/classes/Dialogue.cs
+++ PoP/PoP/classes/Dialogue.cs
@@ -11,11 +11,26 @@ namespace PoP.classes
 {
     class Dialogue : Location
     {
+        /// <summary>
+        /// A dictionary that represents the conversation in the game, mapping each dialogue ID to its corresponding text.
+        /// </summary>
         public Dictionary<string, string> conversation;
 
+        /// <summary>
+        /// The index of the current line of dialogue being displayed.
+        /// </summary>
         public int dialogueIndex = 0;
+
         private int personID;
 
+        /// <summary>
+        /// Initializes a new instance of the Dialogue class with the specified ID, coordinates, file path, and name.
+        /// </summary>
+        /// <param name="id">The ID of the dialogue.</param>
+        /// <param name="x">The X coordinate of the dialogue.</param>
+        /// <param name="y">The Y coordinate of the dialogue.</param>
+        /// <param name="path">The file path of the JSON file containing the dialogue data.</param>
+        /// <param name="name">The name of the dialogue.</param>
         public Dialogue(int id, int x, int y, string path, string name) : base(id, x, y)
         {
             this.id = id;
@@ -28,7 +43,10 @@ namespace PoP.classes
             string json = File.ReadAllText(path);
             conversation = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
         }
-        
+
+        /// <summary>
+        /// Sets the <see cref="Map"/>.CurrentLocation to this instance and starts the Dialogue
+        /// </summary>
         public override void LoadLocation()
         {
             GameLoop.display.WipeTextBox();
@@ -38,6 +56,9 @@ namespace PoP.classes
             Start();
         }
 
+        /// <summary>
+        /// Starts the dialogue.
+        /// </summary>
         public override void Start()
         {
             KeyboardInput.KeyPressed += KeyPressed;
@@ -46,17 +67,23 @@ namespace PoP.classes
 
         public void KeyPressed(ConsoleKey key)
         {
+            // Check if the game is currently in the dialogue phase and if the spacebar key was pressed
             if (GameLoop.Phase == GamePhase.DIALOGUE && key == ConsoleKey.Spacebar)
             {
+
+                // Increment the dialogue index to display the next line
                 dialogueIndex += 1;
 
+                //Check whether if it is the end of the conversation
                 if(dialogueIndex < conversation.Count)
                 {
+                    // Clear the text box and display the next line of dialogue
                     GameLoop.display.WipeTextBox();
                     GameLoop.display.DrawConversation(conversation[dialogueIndex.ToString()], 4, 50, Name, personID++);
                 }
                 else
                 {
+                    // Clear the text box and end the dialogue phase
                     GameLoop.display.WipeTextBox();
                     End();
                 }
