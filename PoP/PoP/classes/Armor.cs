fb003039138031c10ffb4a11534b002aa11cccfe
Megyesi Máté <megyesimate@gmail.com>
2023-04-21 15:17:31 +0200

    Armor doc

--- PoP/PoP/classes/Armor.cs
+++ PoP/PoP/classes/Armor.cs
@@ -8,8 +8,18 @@ namespace PoP.classes
 {
     class Armor : Item
     {
+        /// <summary>
+        /// The Defense property represents a float value that represents how many percent of the damage it can negate.
+        /// </summary>
         public float Defense { get; set; }
 
+        /// <summary>
+        /// Creates a new armor with the specified name, type, and defense.
+        /// </summary>
+        /// <param name="name">The name of the armor.</param>
+        /// <param name="armorType">The type of armor (head, chest, or leg).</param>
+        /// <param name="defense">The amount of defense the armor provides.</param>
+        /// <exception cref="ArgumentException">Thrown if an invalid armor type is specified.</exception>
         public Armor(string name,Slot armorType, float defense)
         {
             switch (armorType)
@@ -26,18 +36,31 @@ namespace PoP.classes
             Defense = defense;
         }
 
+        /// <summary>
+        /// Adds the item to the player's inventory if there is space available.
+        /// </summary>
         public override void Collect()
         {
-            if(Inventory.inventory.Count < Inventory.inventoryLimit)
+            // Check if the inventory has space for the item.
+            if (Inventory.inventory.Count < Inventory.inventoryLimit)
             {
+                // Add the item to the inventory.
                 Inventory.inventory.Add(this);
+
+                // Update the display to show the current inventory count.
                 GameLoop.display.DrawString($"INVENTORY({Inventory.inventory.Count})", 212, 1);
             }
         }
 
+        /// <summary>
+        /// Removes the item from the player's inventory.
+        /// </summary>
         public override void Drop()
         {
+            // Remove the item from the inventory.
             Inventory.inventory.Remove(this);
+
+            // Update the display to show the current inventory count.
             GameLoop.display.DrawString($"INVENTORY({Inventory.inventory.Count})", 212, 1);
         }
 
