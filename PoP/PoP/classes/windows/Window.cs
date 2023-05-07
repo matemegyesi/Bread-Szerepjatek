1716075a0176e90b3ce211940f599d47da932deb
Gulyás Csaba <neugulyascsaba@gmail.com>
2023-05-07 07:11:39 +0200

    Window dokumentációja

--- PoP/PoP/classes/windows/Window.cs
+++ PoP/PoP/classes/windows/Window.cs
@@ -17,6 +17,9 @@ namespace PoP.classes.windows
         protected List<string> LineList = new List<string>();
         public List<string> CachedLineList { get; set; } = new List<string>();
 
+        /// <summary>
+        /// It gives a list of the lines of the window.
+        /// </summary>
         public List<string> GetLines
         {
             get
@@ -50,6 +53,10 @@ namespace PoP.classes.windows
             }
         }
 
+        /// <summary>
+        /// Generates the uncached or changed parts of the window.
+        /// </summary>
+        /// <returns>A list of the lines of the window.</returns>
         protected virtual List<string> GenerateLines()
         {
             LineList.Clear();
@@ -59,16 +66,28 @@ namespace PoP.classes.windows
             return LineList;
         }
 
+        /// <summary>
+        /// Appends a line full of spaces to the bottom of the window.
+        /// </summary>
         protected virtual void AddBlankLine()
         {
             LineList.Add(Style.GetBlankLine(Width));
         }
 
+        /// <summary>
+        /// Appends a line full of spaces to the bottom of the window.
+        /// </summary>
+        /// <param name="localList">The local list that will be changed.</param>
         protected virtual void AddBlankLineLocal(ref List<string> localList)
         {
             localList.Add(Style.GetBlankLine(Width));
         }
 
+        /// <summary>
+        /// Appends a line (or lines if the text is too long) to the bottom of the window.
+        /// </summary>
+        /// <param name="text">The content of the line(s).</param>
+        /// <param name="hasMargin">Optionally adds an indent to the beginning of the line(s).</param>
         protected virtual void AddLine(string text, bool hasMargin = false) // If the text is multi-line long, then the formatting will be removed. // Don't use: \n \t
         {
             string textAnsiPurged = Style.PurgeAnsi(text);
@@ -90,6 +109,12 @@ namespace PoP.classes.windows
             }
         }
 
+        /// <summary>
+        /// Appends a line (or lines if the text is too long) to the bottom of a local list.
+        /// </summary>
+        /// <param name="localList">The local list that will be changed.</param>
+        /// <param name="text">The content of the line(s).</param>
+        /// <param name="hasMargin">Optionally adds an indent to the beginning of the line(s).</param>
         protected virtual void AddLineLocal(ref List<string> localList, string text, bool hasMargin = false)
         {
             string textAnsiPurged = Style.PurgeAnsi(text);
@@ -111,11 +136,21 @@ namespace PoP.classes.windows
             }
         }
 
+        /// <summary>
+        /// Inserts a line full of spaces to the specified index of the window.
+        /// </summary>
+        /// <param name="index">The index of where it should be inserted to.</param>
         protected virtual void InsertBlankLine(int index)
         {
             LineList.Insert(index, Style.GetBlankLine(Width));
         }
 
+        /// <summary>
+        /// Inserts a line (or lines if the text is too long) to the specified index of the window.
+        /// </summary>
+        /// <param name="index">The index of where it should be inserted to.</param>
+        /// <param name="text">The content of the line(s).</param>
+        /// <param name="hasMargin">Optionally adds an indent to the beginning of the line(s).</param>
         protected virtual void InsertLine(int index, string text, bool hasMargin = false)
         {
             string textAnsiPurged = Style.PurgeAnsi(text);
@@ -137,6 +172,12 @@ namespace PoP.classes.windows
             }
         }
 
+        /// <summary>
+        /// Inserts a range of lines starting from the specified index of the window.
+        /// </summary>
+        /// <param name="index">The index of where it should be inserted to.</param>
+        /// <param name="textRange">The content of the lines.</param>
+        /// <param name="hasMargin">Optionally adds an indent to the beginning of the line(s).</param>
         protected virtual void InsertLineRange(int index, List<string> textRange, bool hasMargin = false)
         {
             for (int i = textRange.Count - 1; i >= 0; i--)
@@ -161,6 +202,9 @@ namespace PoP.classes.windows
             }
         }
 
+        /// <summary>
+        /// It forces the window to look for changes at the next render.
+        /// </summary>
         public void ForceUpdate()
         {
             HasChanged = true;
