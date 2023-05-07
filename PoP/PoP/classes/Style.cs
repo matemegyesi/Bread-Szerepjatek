fac383f344dee288931a638a389008c30c606874
Gulyás Csaba <neugulyascsaba@gmail.com>
2023-05-07 06:32:16 +0200

    Több Style metódus

--- PoP/PoP/classes/Style.cs
+++ PoP/PoP/classes/Style.cs
@@ -57,6 +57,9 @@ namespace PoP.classes
     internal class Style
     {
 
+        /// <summary>
+        /// Gives back the ANSI escape code that stops every styling.
+        /// </summary>
         private const string END = "\u001b[0m";
 
 
@@ -87,6 +90,52 @@ namespace PoP.classes
             }
         }
 
+        /// <summary>
+        /// Surrounds the character with ANSI escape code, changing its color.
+        /// </summary>
+        /// <param name="character">The character that needs to be stylized.</param>
+        /// <param name="color">Enum of the color.</param>
+        public static string Color(char character, ColorAnsi color)
+        {
+            try
+            {
+                string formattedText = AnsiColor((byte)color) + character + END;
+                return formattedText;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Surrounds the number with ANSI escape code, changing its color.
+        /// </summary>
+        /// <param name="number">The number that needs to be stylized.</param>
+        /// <param name="color">Enum of the color.</param>
+        public static string Color(int number, ColorAnsi color)
+        {
+            try
+            {
+                string formattedText = AnsiColor((byte)color) + number.ToString() + END;
+                return formattedText;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Creates an ANSI escape code of the parameter.
+        /// </summary>
+        /// <param name="color">Enum of the color.</param>
+        /// <returns>ANSI escape code of the specified color.</returns>
+        public static string GetColor(ColorAnsi color)
+        {
+            return AnsiColor((byte)color);
+        }
+
         // Formatting
 
         /// <summary>
@@ -105,6 +154,52 @@ namespace PoP.classes
             {
                 return string.Empty;
             }
+        }
+
+        /// <summary>
+        /// Surrounds the character with ANSI escape code, changing its format.
+        /// </summary>
+        /// <param name="character">The character that needs to be stylized.</param>
+        /// <param name="format">Enum of the format.</param>
+        public static string Format(char character, FormatAnsi format)
+        {
+            try
+            {
+                string formattedText = AnsiFormat((byte)format) + character + END;
+                return formattedText;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Surrounds the number with ANSI escape code, changing its format.
+        /// </summary>
+        /// <param name="number">The number that needs to be stylized.</param>
+        /// <param name="format">Enum of the format.</param>
+        public static string Format(int number, FormatAnsi format)
+        {
+            try
+            {
+                string formattedText = AnsiFormat((byte)format) + number.ToString() + END;
+                return formattedText;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Creates an ANSI escape code of the parameter.
+        /// </summary>
+        /// <param name="format">Enum of the format.</param>
+        /// <returns>ANSI escape code of the specified format.</returns>
+        public static string GetFormat(FormatAnsi format)
+        {
+            return AnsiFormat((byte)format);
         }
 
         // Coloring & Formatting
@@ -127,8 +222,291 @@ namespace PoP.classes
                 return string.Empty;
             }
         }
-        
 
+        /// <summary>
+        /// Surrounds the character with ANSI escape code, changing its color and format.
+        /// </summary>
+        /// <param name="character">The character that needs to be stylized.</param>
+        /// <param name="color">Enum of the color.</param>
+        /// <param name="format">Enum of the format.</param>
+        public static string ColorFormat(char character, ColorAnsi color, FormatAnsi format)
+        {
+            try
+            {
+                string formattedText = AnsiColor((byte)color) + AnsiFormat((byte)format) + character + END;
+                return formattedText;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Surrounds the number with ANSI escape code, changing its color and format.
+        /// </summary>
+        /// <param name="number">The number that needs to be stylized.</param>
+        /// <param name="color">Enum of the color.</param>
+        /// <param name="format">Enum of the format.</param>
+        public static string ColorFormat(int number, ColorAnsi color, FormatAnsi format)
+        {
+            try
+            {
+                string formattedText = AnsiColor((byte)color) + AnsiFormat((byte)format) + number.ToString() + END;
+                return formattedText;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        // Aligning
+
+        /// <summary>
+        /// [OBSOLETE] Gives back the space before and after the text that is aligned in the center.
+        /// </summary>
+        /// <param name="text">The text that needs to be aligned in the center.</param>
+        /// <param name="lineLength">The length of the line.</param>
+        /// <returns>An AlignedText class.</returns>
+        public static AlignedText AlignCenterSpaces(string text, int lineLength)
+        {
+            int spaceBefore = (lineLength - text.Length) / 2;
+            int spaceAfter = lineLength - spaceBefore - text.Length;
+
+            AlignedText currentAlignedText = new AlignedText();
+            for (int _ = 0; _ < spaceBefore; _++)
+            {
+                currentAlignedText.Before += ' ';
+            }
+            for (int _ = 0; _ < spaceAfter; _++)
+            {
+                currentAlignedText.After += ' ';
+            }
+
+            return currentAlignedText;
+        }
+
+        /// <summary>
+        /// Gives back the remaining space of a line that already has some text taking up space of it.
+        /// </summary>
+        /// <param name="text">The text that takes up some space.</param>
+        /// <param name="lineLength">The length of the line.</param>
+        /// <returns>A string of spaces.</returns>
+        public static string GetRemainingSpace(string text, int lineLength)
+        {
+            string remainingText = string.Empty;
+            for (int _ = 0; _ < lineLength - text.Length; _++)
+            {
+                remainingText += ' ';
+            }
+            return remainingText;
+        }
+
+        /// <summary>
+        /// Gives back the remaining space of a line that already has some text taking up space of it.
+        /// </summary>
+        /// <param name="textLength">The amount of space that the text that takes up.</param>
+        /// <param name="lineLength">The length of the line.</param>
+        /// <returns>A string of spaces.</returns>
+        public static string GetRemainingSpace(int textLength, int lineLength)
+        {
+            string remainingText = string.Empty;
+            for (int _ = 0; _ < lineLength - textLength; _++)
+            {
+                remainingText += ' ';
+            }
+            return remainingText;
+        }
+
+        // Line generating
+
+        /// <summary>
+        /// Gives back a line with the specified length made up of spaces.
+        /// </summary>
+        /// <param name="lineLength">The length of the line.</param>
+        /// <returns>The blank line.</returns>
+        public static string GetBlankLine(int lineLength)
+        {
+            string newLine = string.Empty;
+            for (int _ = 0; _ < lineLength; _++)
+            {
+                newLine += ' ';
+            }
+            return newLine;
+        }
+
+        /// <summary>
+        /// Gives back a line with the specified length of the specified character.
+        /// </summary>
+        /// <param name="lineLength">The length of the line.</param>
+        /// <param name="fill">The cahracter that the line should be filled up with.</param>
+        /// <returns>The filled line.</returns>
+        public static string GetBlankLine(int lineLength, char fill)
+        {
+            string newLine = string.Empty;
+            for (int _ = 0; _ < lineLength; _++)
+            {
+                newLine += fill;
+            }
+            return newLine;
+        }
+
+        /// <summary>
+        /// Gives back a dashed line.
+        /// </summary>
+        /// <param name="lineLength">The length of the line.</param>
+        /// <returns>Dashed line.</returns>
+        public static string GetDashedLine(int lineLength)
+        {
+            char border = '-';
+
+            string newLine = string.Empty;
+            if (lineLength % 2 != 0)
+            {
+                for (int i = 0; i < lineLength; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        newLine += ' ';
+                    }
+                    else
+                    {
+                        newLine += border;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < lineLength; i++)
+                {
+                    if (i == lineLength / 2)
+                    {
+                        if (lineLength % 4 == 0)
+                        {
+                            newLine += border;
+                        }
+                        else
+                        {
+                            newLine += ' ';
+                        }
+                    }
+                    else if (i < lineLength / 2)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            newLine += ' ';
+                        }
+                        else
+                        {
+                            newLine += border;
+                        }
+                    }
+                    else if (i > lineLength / 2)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            newLine += border;
+                        }
+                        else
+                        {
+                            newLine += ' ';
+                        }
+                    }
+                }
+            }
+
+            return newLine;
+        }
+
+        /// <summary>
+        /// Word wraps the text.
+        /// </summary>
+        /// <param name="text">The text that is longer than the width.</param>
+        /// <param name="width">The max width of a line.</param>
+        /// <param name="hasMargin">Adds indent to the beginning of the texts.</param>
+        /// <returns>The word wrapped list of text.</returns>
+        public static List<string> BreakLine(string text, int width, bool hasMargin = false)
+        {
+            List<string> brokenText = new List<string>();
+            string[] splitText = text.Split(' ');
+
+            string currentLine = hasMargin ? " " : "";
+            for (int i = 0; i < splitText.Length; i++)
+            {
+                if (splitText[i].Length > width)
+                {
+                    brokenText.Add(splitText[i].Substring(0, width));
+                }
+                else if (currentLine.Length + splitText[i].Length + 1 <= width)
+                {
+                    currentLine += splitText[i] + ' ';
+                }
+                else
+                {
+                    brokenText.Add(currentLine);
+                    currentLine = hasMargin ? " " : "";
+                    currentLine += splitText[i] + ' ';
+                }
+
+                if (i == splitText.Length - 1)
+                {
+                    brokenText.Add(currentLine);
+                }
+            }
+
+            return brokenText;
+        }
+
+        // Spacing
+
+        /// <summary>
+        /// Adds a space to the beginning and to the ending of the text.
+        /// </summary>
+        /// <param name="text">The text that needs to be padded.</param>
+        /// <returns>The padded text.</returns>
+        public static string AddPadding(string text)
+        {
+            return ' ' + text + ' ';
+        }
+
+        // Unholy act...
+
+        /// <summary>
+        /// Exterminates the ANSI escape code from the specified text.
+        /// </summary>
+        /// <param name="corruptedText">The text plagued with heresy.</param>
+        /// <returns>Holy text.</returns>
+        public static string PurgeAnsi(string corruptedText)
+        {
+            if (corruptedText.Contains("\u001b"))
+            {
+                string newText = string.Empty;
+                bool notAnsi = true;
+                for (int i = 0; i < corruptedText.Length; i++)
+                {
+                    if (char.IsControl(corruptedText[i]))
+                    {
+                        notAnsi = false;
+                    }
+                    else if (corruptedText[i] == 'm' && !notAnsi)
+                    {
+                        notAnsi = true;
+                    }
+                    else if (notAnsi)
+                    {
+                        newText += corruptedText[i];
+                    }
+                }
+
+                return newText;
+            }
+            else
+            {
+                return corruptedText;
+            }
+        }
+        
         #endregion
 
 
