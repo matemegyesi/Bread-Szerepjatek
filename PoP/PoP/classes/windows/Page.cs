ff43364d51ea3f74049438853ad66fa396d9b967
Gulyás Csaba <neugulyascsaba@gmail.com>
2023-05-07 07:05:20 +0200

    Page class alapok

---
+++ PoP/PoP/classes/windows/Page.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoP.classes.windows
+{
+    internal class Page
+    {
+        public static List<Page> List = new List<Page>();
+        public static int AvailableSpace;
+
+        public List<Item> ContainedItems { get; set; } = new List<Item>();
+        private int remainingSpace;
+
+        public Page()
+        {
+            remainingSpace = AvailableSpace;
+        }
+    }
+}
