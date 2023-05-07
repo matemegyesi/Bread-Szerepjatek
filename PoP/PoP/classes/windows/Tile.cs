ffec1a5fc8135b2c6d4b304b4728d76a17d0efd1
Gulyás Csaba <neugulyascsaba@gmail.com>
2023-05-07 07:06:26 +0200

    Tile létrehozása

---
+++ PoP/PoP/classes/windows/Tile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoP.classes.windows
+{
+    internal class Tile
+    {
+        public char Char { get; set; }
+        public string StartStyle { get; private set; }
+        public string EndStyle { get; private set; }
+
+        public Tile(char character)
+        {
+            Char = character;
+            StartStyle = string.Empty;
+            EndStyle = string.Empty;
+        }
+
+        public void SetStartStyle(string style)
+        {
+            StartStyle = style;
+        }
+
+        public void SetEndStyle(string style)
+        {
+            EndStyle = style;
+        }
+
+        public void ResetStyle()
+        {
+            StartStyle = string.Empty;
+            EndStyle = string.Empty;
+        }
+    }
+}
