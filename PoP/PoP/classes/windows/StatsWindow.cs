9cee4d9c3940f0666dc1e2bc4fce4b5ef182e9a8
Gulyás Csaba <neugulyascsaba@gmail.com>
2023-05-07 07:30:11 +0200

    StatsWindow generálás

--- PoP/PoP/classes/windows/StatsWindow.cs
+++ PoP/PoP/classes/windows/StatsWindow.cs
@@ -21,5 +21,35 @@ namespace PoP.classes.windows
             Height = 23;
             Width = 44;
         }
+
+        protected override List<string> GenerateLines()
+        {
+            LineList.Clear();
+
+            // Stats header
+            AddBlankLine();
+            AlignedText _header = Style.AlignCenterSpaces(Title, Width);
+            AddLine(_header.Before + Style.ColorFormat(Title, ColorAnsi.WHITE, FormatAnsi.HIGHLIGHT) + _header.After);
+
+            AddBlankLine();
+            AddLine(Style.Color(Style.GetDashedLine(Width), ColorAnsi.GREY));
+
+            AddBlankLine();
+
+            // Fills the remaining lines
+            int remainingLineCount = Height - LineList.Count;
+            for (int i = 0; i < remainingLineCount; i++)
+            {
+                AddBlankLine();
+            }
+
+            // Last resort: deletes additional lines
+            if (LineList.Count > Height)
+            {
+                LineList.RemoveRange(Height, LineList.Count - Height);
+            }
+
+            return LineList;
+        }
     }
 }
